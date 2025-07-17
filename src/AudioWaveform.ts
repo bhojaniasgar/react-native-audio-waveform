0563269b4862a8ad6715a29ba86d2c6ca3881022
asgarBhojani <im.bhojaniasgar@gmail.com>
2025-07-17 12:02:43 +0530

    chore(package): rename to @bhojaniasgar/react-native-audio-waveform

--- src/AudioWaveform.ts
+++ src/AudioWaveform.ts
@@ -2,7 +2,7 @@ import { NativeModules, Platform } from 'react-native';
 import type { IAudioWaveforms } from './types';
 
 const LINKING_ERROR =
-  "The package 'react-native-audio-waveform' doesn't seem to be linked. Make sure: \n\n" +
+  "The package '@bhojaniasgar/react-native-audio-waveform' doesn't seem to be linked. Make sure: \n\n" +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
